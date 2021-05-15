51d1919ee8bfce539251390a4cee25b160ee674f
leehwasub <hwasub1115@naver.com>
2021-05-15 19:53:28 +0900

    #03 Tile Map データ保存
    #03 Tile Map 데이터 저장

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class MapData
+{
+    public Vector2Int mapSize;
+    public int[] mapData;
+
+    public Vector2Int playerPosition;
+}
--- Assets/Scripts/Tilemap2D.cs
+++ Assets/Scripts/Tilemap2D.cs
@@ -14,9 +14,14 @@ public class Tilemap2D : MonoBehaviour
     [SerializeField]
     private TMP_InputField inputHeight; // 맵의 height 크기를 얻어오는 InputField
 
+    private MapData mapData; // 맵 데이터 저장에 사용되는 데이터 양식 클래스
+
     // 맵x,y 크기 프로퍼티
     public int Width { private set; get; } = 10;
     public int Height { private set; get; } = 10;
+
+    // 맵에 배치되는 타일 정보 저장을 위한 List 프로퍼티
+    public List<Tile> TileList { private set; get; }
 
     private void Awake()
     {
@@ -24,7 +29,8 @@ public class Tilemap2D : MonoBehaviour
         inputWidth.text = Width.ToString();
         inputHeight.text = Height.ToString();
 
-        //GenerateTilemap();
+        mapData = new MapData();
+        TileList = new List<Tile>();
     }
 
     public void GenerateTilemap()
@@ -50,6 +56,10 @@ public class Tilemap2D : MonoBehaviour
                 SpawnTile(TileType.Empty, position);
             }
         }
+
+        mapData.mapSize.x = Width;
+        mapData.mapSize.y = Height;
+        mapData.mapData = new int[TileList.Count];
     }
 
     private void SpawnTile(TileType tileType, Vector3 position)
@@ -60,6 +70,33 @@ public class Tilemap2D : MonoBehaviour
 
         Tile tile = clone.GetComponent<Tile>(); // 방금 생성한 타일(clone) 오브젝트의 Tile.Setup() 메소드 호출
         tile.Setup(tileType);
+
+        TileList.Add(tile); // 모든 타일 정보는 tileList 리스트에 보관
+    }
+
+    public MapData GetMapData()
+    {
+        //맵에 배치된 모든 타일의 정보를 mapData.mapData 배열에 저장
+        for(int i = 0; i < TileList.Count; i++)
+        {
+            if(TileList[i].TileType != TileType.Player)
+            {
+                mapData.mapData[i] = (int)TileList[i].TileType;
+            }
+            // 현재 위치의 타일이 플레이어면
+            else
+            {
+                // 현재 위치의 타일은 빈 타일(Empty)로 설정
+                mapData.mapData[i] = (int)TileType.Empty;
+
+                // 현재 위치 정보를 mapData.playerPosition에 저장
+                int x = (int)TileList[i].transform.position.x;
+                int y = (int)TileList[i].transform.position.y;
+                mapData.playerPosition = new Vector2Int(x, y);
+            }
+        }
+
+        return mapData;
     }
 
 
